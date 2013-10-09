67e1e5229f33a6244cd8f8cd9ea9021f53973410
Luke Hamilton <lukemhamilton@gmail.com>
2013-10-09 00:05:31 -0400

    cross browser logging

---
+++ testing_and_debugging/cross_browser_logging.js
@@ -0,0 +1,16 @@
+// Cross Browser Console Log
+
+function log() {
+  try {
+    // Try to log message using most common browser method
+    console.log.apply(console, arguments);
+  } catch (e) { // Catch any failure in logging
+    try {
+      // Try to log the Opera way
+      opera.postError.apply(opera, arguments);
+    } catch (e) {
+      // Use alert if all else fails
+      alert(Array.prototype.join.call( arguments, " "));
+    }
+  }
+}
